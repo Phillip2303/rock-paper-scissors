a728534925bc4a2c4281d8fdc4dc360f40b7f335
Phillip2303 <142237061+Phillip2303@users.noreply.github.com>
2025-08-06 13:18:58 +0200

    Write a logic to play the entire game
    Move playRound() and the variables into playGame() method and display score

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-let humanScore = 0;
-let computerScore = 0;
-
 function getComputerChoice() {
   randomNum = Math.floor(Math.random() * 3);
   if (randomNum == 0) {
@@ -17,7 +14,7 @@ function getHumanChoice() {
   return input.toLowerCase();
 }
 
-function playRound(humanChoice, computerChoice) {
+/*function playRound(humanChoice, computerChoice) {
   console.log("Human: " + humanChoice + "\nComputer: " + computerChoice);
   if (humanChoice == computerChoice) {
     console.log(
@@ -48,9 +45,50 @@ function playRound(humanChoice, computerChoice) {
     );
     humanScore++;
   }
+}*/
+
+function playGame() {
+  let humanScore = 0;
+  let computerScore = 0;
+
+  let playRound = function (humanChoice, computerChoice) {
+    //console.log("Human: " + humanChoice + "\nComputer: " + computerChoice);
+    if (humanChoice == computerChoice) {
+      console.log(
+        "Draw! Both used " + humanChoice[0].toUpperCase() + humanChoice.slice(1)
+      );
+    } else if (
+      (humanChoice == "rock" && computerChoice == "paper") ||
+      (humanChoice == "paper" && computerChoice == "scissors") ||
+      (humanChoice == "scissors" && computerChoice == "rock")
+    ) {
+      console.log(
+        "You lose! " +
+          computerChoice[0].toUpperCase() +
+          computerChoice.slice(1) +
+          " beats " +
+          humanChoice[0].toUpperCase() +
+          humanChoice.slice(1)
+      );
+      computerScore++;
+    } else {
+      console.log(
+        "You win! " +
+          humanChoice[0].toUpperCase() +
+          humanChoice.slice(1) +
+          " beats " +
+          computerChoice[0].toUpperCase() +
+          computerChoice.slice(1)
+      );
+      humanScore++;
+    }
+  };
+
+  for (i = 0; i < 5; i++) {
+    playRound(getHumanChoice(), getComputerChoice());
+  }
+
+  console.log(`Player: ${humanScore}\nComputer: ${computerScore}`);
 }
 
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
-
-playRound(humanSelection, computerSelection);
+playGame();

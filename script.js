99f136c38bd892a07a73c4cee3a34dd6df3d1e4b
Phillip2303 <142237061+Phillip2303@users.noreply.github.com>
2025-08-06 12:46:52 +0200

    Write the logic to play a single round
    Use lots of if conditions, increment scoreand display winner

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ let computerScore = 0;
 
 function getComputerChoice() {
   randomNum = Math.floor(Math.random() * 3);
-  console.log(randomNum);
   if (randomNum == 0) {
     return "rock";
   } else if (randomNum == 1) {
@@ -15,5 +14,43 @@ function getComputerChoice() {
 
 function getHumanChoice() {
   input = prompt("Choose between rock, paper and scissors!");
-  return input;
+  return input.toLowerCase();
 }
+
+function playRound(humanChoice, computerChoice) {
+  console.log("Human: " + humanChoice + "\nComputer: " + computerChoice);
+  if (humanChoice == computerChoice) {
+    console.log(
+      "Draw! Both used " + humanChoice[0].toUpperCase() + humanChoice.slice(1)
+    );
+  } else if (
+    (humanChoice == "rock" && computerChoice == "paper") ||
+    (humanChoice == "paper" && computerChoice == "scissors") ||
+    (humanChoice == "scissors" && computerChoice == "rock")
+  ) {
+    console.log(
+      "You lose! " +
+        computerChoice[0].toUpperCase() +
+        computerChoice.slice(1) +
+        " beats " +
+        humanChoice[0].toUpperCase() +
+        humanChoice.slice(1)
+    );
+    computerScore++;
+  } else {
+    console.log(
+      "You win! " +
+        humanChoice[0].toUpperCase() +
+        humanChoice.slice(1) +
+        " beats " +
+        computerChoice[0].toUpperCase() +
+        computerChoice.slice(1)
+    );
+    humanScore++;
+  }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
